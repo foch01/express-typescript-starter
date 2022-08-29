8ced9b0ca9d16db326e06c8a73453932f50a6598
Thomas Sanceo <tsanceo@yahoo.com>
2022-08-29 14:16:42 +0200

    Revert "feature: Add puppeteer scraping"
    This reverts commit f165d4adf893ee279ab93ba3ca87e86943132c51.

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,8 @@
 import express from 'express'
-const puppeteer = require('puppeteer');
 import * as welcomeController from "./controllers/welcome";
-import CookieService from './services/cookie.service';
-const cron = require('node-cron');
 
 const app = express();
 
-const task = cron.schedule("* 0 * * * *", async () =>  {
-    console.log('start')
-    await getCookiesFromApi();
-}, {
-    scheduled: false
-});
-
-task.start();
-
-async function getCookiesFromApi(): Promise<void> {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://note-pad.net/ru/read/e74b79888eb748dbf06f5f6f1b0b9c1f?page=1', { waitUntil: "domcontentloaded" });
-    await page.waitForSelector('#page_text_ifr');
-    const elementHandle = await page.$('#page_text_ifr');
-    const frame = await elementHandle.contentFrame();
-    const text = await frame.$eval('#tinymce > p', (element: Element) => element.textContent);
-    await CookieService.createOneCustom(text);
-
-    await browser.close();
-}
-
 app.get('/', welcomeController.welcome);
-app.get('/cookie', welcomeController.cookie);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,8 @@
-import mongoose from 'mongoose';
+import express from 'express';
 import app from './app'
 const PORT = 8000;
 
-mongoose
-    .connect(`${process.env.MONGO_URI}`)
-    .then(() => {
-        console.log('Connect successful to DB MONGO');
-    })
-    .catch((err) => {
-        console.log(`MongoDb connection error. Please make sureDb is running. ${err}`);
-    });
 
 app.listen(PORT, () => {
-    console.log(`⚡️  Server is running at http://localhost:${PORT}`);
+    console.log(`⚡️  Server is running at https://localhost:${PORT}`);
 });
